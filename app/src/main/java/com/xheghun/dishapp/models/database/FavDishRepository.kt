e2eb5e4b99481938a938a0bcfdf13c8a034132a0
david <davidr@pointzi.com>
2022-01-16 23:50:39 +0100

    add repo

---
+++ app/src/main/java/com/xheghun/dishapp/models/database/FavDishRepository.kt
@@ -0,0 +1,12 @@
+package com.xheghun.dishapp.models.database
+
+import androidx.annotation.WorkerThread
+import com.xheghun.dishapp.models.entities.FavDish
+
+class FavDishRepository(private val favDishDao: FavDishDao) {
+
+    @WorkerThread
+    suspend fun insertFavDiah(favDish: FavDish) {
+        favDishDao.insertFavDishDetails(favDish)
+    }
+}
