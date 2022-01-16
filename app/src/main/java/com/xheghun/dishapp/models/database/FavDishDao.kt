c890fe52550e5280956356b72ce1a64e6d8fdbc7
david <davidr@pointzi.com>
2022-01-16 23:44:41 +0100

    database setup

--- app/src/main/java/com/xheghun/dishapp/models/database/FavDishDao.kt
+++ app/src/main/java/com/xheghun/dishapp/models/database/FavDishDao.kt
@@ -1,4 +1,12 @@
 package com.xheghun.dishapp.models.database
 
+import androidx.room.Dao
+import androidx.room.Insert
+import com.xheghun.dishapp.models.entities.FavDish
+
+@Dao
 interface FavDishDao {
+
+    @Insert
+    suspend fun insertFavDishDetails(favDish: FavDish)
 }
---
+++ app/src/main/java/com/xheghun/dishapp/models/database/FavDishDatabase.kt
@@ -0,0 +1,32 @@
+package com.xheghun.dishapp.models.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.xheghun.dishapp.models.entities.FavDish
+
+@Database(entities = arrayOf(FavDish::class), version = 1)
+abstract class FavDishDatabase: RoomDatabase() {
+    companion object {
+        @Volatile
+        private var INSTANCE: FavDishDatabase? = null
+
+        /// create FavDishDatabase Singleton
+        fun getDatabase(context: Context): FavDishDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    FavDishDatabase::class.java,
+                    "fav_dish_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+
+                INSTANCE = instance
+
+                instance
+            }
+        }
+    }
+}
