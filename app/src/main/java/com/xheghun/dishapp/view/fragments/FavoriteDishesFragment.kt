983181a4a51210f53baa2deb891b3e3ede662071
david <davidr@pointzi.com>
2022-01-21 02:44:29 +0100

    minor refactor

--- app/src/main/java/com/xheghun/dishapp/view/fragments/FavoriteDishesFragment.kt
+++ app/src/main/java/com/xheghun/dishapp/view/fragments/FavoriteDishesFragment.kt
@@ -8,13 +8,14 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.xheghun.dishapp.databinding.FragmentDashboardBinding
+import com.xheghun.dishapp.databinding.FragmentAllDishBinding
+import com.xheghun.dishapp.databinding.FragmentFavDishesBinding
 import com.xheghun.dishapp.viewmodel.DashboardViewModel
 
 class FavoriteDishesFragment : Fragment() {
 
     private lateinit var dashboardViewModel: DashboardViewModel
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentFavDishesBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -28,7 +29,7 @@ class FavoriteDishesFragment : Fragment() {
         dashboardViewModel =
             ViewModelProvider(this).get(DashboardViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentFavDishesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
